8d9c516ea0f30690df1ca8fd8e7030db7cc7a76e
prafdin <pravdin.biss@gmail.com>
2025-02-12 00:23:12 +0400

    First try

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,30 @@
 package ru.prafdin.todolik
 
+import scala.util.{CommandLineParser, Try}
+
+given CommandLineParser.FromString[Option[String]] with
+    def fromString(value: String): Option[String] = Some(value)
+
+def parseTaskNum(args: Seq[String]): Int =
+    args.headOption.flatMap(a => Try(a.toInt).toOption)
+        .getOrElse(throw new IllegalStateException("Номер заметки должен быть числом"))
 
 @main
-def main(): Unit =
-  println("Hello world")
+def main(action: String, args: String*): Unit = action match
+    case "list" =>
+        println("Will list tasks")
+
+    case "create" =>
+        val title = args.headOption.getOrElse(
+            throw new IllegalStateException("Необходимо передать название новой заметки")
+        )
+        println(s"Will create new one with $title")
+
+    case "delete" =>
+        println(s"Will delete task number of ${parseTaskNum(args)}")
+
+    case "update" =>
+        println(s"Will update task number of ${parseTaskNum(args)}")
+
+    case _ =>
+        throw new IllegalArgumentException(s"Неизвестная команда: $action")
